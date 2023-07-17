1c33ba939ec07731a2c6b8b6bc61b9679cb23b62
Anisur Rahman Mahmud <19202103107@cse.bubt.edu.bd>
2023-07-18 00:45:23 +0600

    Problem Fixed, Error Fixed

---
+++ app/src/main/java/com/example/social_media/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.example.social_media;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        //actionbar and its title
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setTitle("Profile");
+    }
+}
