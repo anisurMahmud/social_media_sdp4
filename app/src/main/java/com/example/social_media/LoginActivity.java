84538ba2e557363a7df71aba3bad58c481b78d6b
Mubtasim-Prime <mubtasimfuadrafiq@gmail.com>
2023-07-21 16:18:28 +0600

    Google Sign in (Final)

--- app/src/main/java/com/example/social_media/LoginActivity.java
+++ app/src/main/java/com/example/social_media/LoginActivity.java
@@ -118,7 +118,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent signInIntent = mGoogleSignInClient.getSignInIntent();
                 startActivityForResult(signInIntent, RC_SIGN_IN);
-
             }
         });
 
